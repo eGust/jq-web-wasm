b7a2f96f1ebf6e4c0350d2f8d27e3201f7d4a6ae
eGust <egustc@gmail.com>
2023-01-29 23:27:19 +1300

    initial commit

---
+++ dist/jq.wasm.d.ts
@@ -0,0 +1,8 @@
+declare module 'jq-web/jq.wasm' {
+  export const json: <R, T = object>(data: T, filter: string) => R;
+  export const raw: (data: string, filter: string) => string;
+  export const promised: {
+    json: <R, T = object>(data: T, filter: string) => Promise<R>,
+    raw: (data: string, filter: string) => Promise<string>,
+  };
+}
